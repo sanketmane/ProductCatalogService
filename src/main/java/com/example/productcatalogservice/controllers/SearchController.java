51ece648f91b3d7acfabbac459af5e139cfb150d
Sanket Mane <sanket.mane@gmail.com>
2025-08-15 16:42:18 +0530

    Adding some comments to Search controller

--- src/main/java/com/example/productcatalogservice/controllers/SearchController.java
+++ src/main/java/com/example/productcatalogservice/controllers/SearchController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// We will create a separate controller for Search functionality
+
 @RestController
 @RequestMapping("/search")
 public class SearchController {
